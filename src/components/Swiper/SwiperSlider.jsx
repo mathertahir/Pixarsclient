4039671d195569e1caecc018704a7b77e372c5a7
MuhammadMather <mather@pixarsart.com>
2023-03-21 16:07:05 +0500

    Swiper section responsiveness

--- src/components/Swiper/SwiperSlider.jsx
+++ src/components/Swiper/SwiperSlider.jsx
@@ -77,7 +77,7 @@ const SwiperSlider = () => {
       </div>
 
       <div className="about-area">
-        <Container className="pt-140 pb-130 position-relative">
+        <Container className="pt-140 pb-130 position-relative ">
           <Row className="mx-0">
             <Col>
               <div className="about-maintitle">
@@ -101,116 +101,18 @@ const SwiperSlider = () => {
                   </div>{" "}
                 </div>
               </div>
-
-              {/* <div className="about-content-main  ">
-                <div className="about-left-img">
-                  <img src="images/about-left.png"></img>
-                </div>
-
-                <div className="about-description position-relative ">
-                  <p>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      At Pixarsart our aim is to create the most effective{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      online presence for your brand. We use the latest{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      technology to provide you with the most intuitive{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      solutions to cater to all your business needs. We want{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      are committed to providing creative and innovative{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      Backend Development, Digital Product Development,{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      Server Management, Continuous Integration and{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      eCommerce solutions. We are progressively working{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      with clients with legacy systems as well as progressive{" "}
-                    </div>
-                    <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                      technologies includingVR and AR.{" "}
-                    </div>
-                  </p>
-
-                  <div className="shape">
-                    <div className="primary"></div>
-                    <div className="secondary"></div>
-                  </div>
-                </div>
-                <div className="about-right-img">
-                  <img src="images/about-right.png"></img>
-                </div>
-              </div> */}
             </Col>
           </Row>
-          {/* 
-          <div className="about-content-main  ">
-            <div className="about-left-img">
-              <img src="images/about-left.png"></img>
-            </div>
-
-            <div className="about-description position-relative ">
-              <p>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  At Pixarsart our aim is to create the most effective{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  online presence for your brand. We use the latest{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  technology to provide you with the most intuitive{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  solutions to cater to all your business needs. We want{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  are committed to providing creative and innovative{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  Backend Development, Digital Product Development,{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  Server Management, Continuous Integration and{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  eCommerce solutions. We are progressively working{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  with clients with legacy systems as well as progressive{" "}
-                </div>
-                <div className="fs-18 cl-gray2 fw-400 f-kanit">
-                  technologies includingVR and AR.{" "}
-                </div>
-              </p>
-
-              <div className="shape">
-                <div className="primary"></div>
-                <div className="secondary"></div>
-              </div>
-            </div>
-            <div className="about-right-img">
-              <img src="images/about-right.png"></img>
-            </div>
-          </div> */}
 
           <Row className="align-items-center  gx-5 mx-0">
-            <Col lg={4} className="">
+            <Col md={4} sm={12} className="">
               <div className="about-left-img  ">
                 <img src="images/about-left.png  " className="w-100"></img>
               </div>
             </Col>
-            <Col lg={8} className="p-40">
-              <Row className="mx-0">
-                <Col lg={9}>
+            <Col md={8} className="p-40">
+              <Row className="mx-0 flex-md-column-reverse   flex-lg-row">
+                <Col md={9} sm={12}>
                   <div className="about-description p-30  ">
                     <p>
                       <div className="fs-18 cl-gray2 fw-400 f-kanit">
@@ -246,8 +148,8 @@ const SwiperSlider = () => {
                     </p>
                   </div>
                 </Col>
-                <Col lg={3} className="p-40  position-relative">
-                  <div>
+                <Col md={3} sm={12} className="p-40  position-relative">
+                  <divn className=" about-right-image-section-main">
                     <div className="about-right-img">
                       <img src="images/about-right.png"></img>
                     </div>
@@ -256,7 +158,7 @@ const SwiperSlider = () => {
                       <div className="primary"></div>
                       <div className="secondary"></div>
                     </div>
-                  </div>
+                  </divn>
                 </Col>
               </Row>
             </Col>
